85a8ed76198d830b7e04b83bbc6d7501b228f7da
dberkay34 <83220351+dberkay34@users.noreply.github.com>
2021-07-05 11:02:25 +0300

    Add files via upload

---
+++ Patika-Veri_Bilimi-Sql_Odev11.sql
@@ -0,0 +1,37 @@
+/*
+KODLUYORUZ - ODEV 11
+
+Merhabalar,
+
+Aşağıdaki sorgu senaryolarını dvdrental örnek veri tabanı üzerinden gerçekleştiriniz.
+
+actor ve customer tablolarında bulunan first_name sütunları için tüm verileri sıralayalım.
+actor ve customer tablolarında bulunan first_name sütunları için kesişen verileri sıralayalım.
+actor ve customer tablolarında bulunan first_name sütunları için ilk tabloda bulunan ancak ikinci tabloda bulunmayan verileri sıralayalım.
+İlk 3 sorguyu tekrar eden veriler için de yapalım.
+Kolay Gelsin.
+
+*/
+--select first_name from actor union select first_name from customer
+--select first_name from actor intersect select first_name from customer
+--select first_name from actor except select first_name from customer
+
+--select first_name from actor union all select first_name from customer
+--select first_name from actor intersect /*all*/ select first_name from customer
+--select first_name from actor except all select first_name from customer
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
